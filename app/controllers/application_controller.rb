7e882377029c51da969984c2f27bca35cde892d9
Erika.K <koguchi.e915@gmail.com>
2024-11-15 17:31:38 +0900

    [Add]11/15 6章途中

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+
+    # configure_permitted_parametersメソッドが
+    # devise利用の機能（ユーザ登録、ログイン認証など）が使われる前に実行される
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    end
 end
