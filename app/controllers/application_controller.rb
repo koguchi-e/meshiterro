4029088b3460d64ed5eccc863b5b966d9dc73148
Erika.K <koguchi.e915@gmail.com>
2024-11-18 11:40:46 +0900

    [Add]11/18 7章終

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,22 @@
 class ApplicationController < ActionController::Base
 
-    # configure_permitted_parametersメソッドが
-    # devise利用の機能（ユーザ登録、ログイン認証など）が使われる前に実行される
-    before_action :configure_permitted_parameters, if: :devise_controller?
+  # サインイン後にどこに遷移するかを設定しているメソッド
+  # (Deviseが用意しているメソッド) 7章
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-    protected
+  # サインイン後にルートパスに遷移しないよう、初期設定を上書きする
+  def after_sign_in_path_for(resource)
+    about_path
+  end
 
-    def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    end
+  # ログアウト後、aboutに転移
+  def after_sign_out_path_for(resource)
+    about_path
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
