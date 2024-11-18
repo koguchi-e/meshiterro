a69068f80fae761faee80e9fd2a2807778ccbbae
Erika.K <koguchi.e915@gmail.com>
2024-11-18 17:54:24 +0900

    [Add]11/18 10章 途中

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,5 +1,14 @@
 class PostImagesController < ApplicationController
   def new
+    @post_image = PostImage.new
+  end
+
+  def create
+    @post_image = PostImage.new(post_image_params)
+    # current_user = ログイン中のユーザーの情報を取得できる便利な記述
+    @post_image.user_id = current_user.id
+    @post_image.save
+    redirect_to post_images_path
   end
 
   def index
@@ -9,5 +18,14 @@ class PostImagesController < ApplicationController
   end
 
   def edit
+  end
+
+  private
+
+  # フォームで入力されたデータが、投稿データとして許可されているパラメータかどうかをチェック
+  # 応用・10章
+  def post_image_params
+    # PostImageモデルへ保存した後、投稿一覧画面へリダイレクト
+    params.require(:post_image).permit(:shop_name, :image, :caption)
   end
 end
