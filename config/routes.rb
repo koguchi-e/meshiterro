c11c543a10f0c9f1bc7fca5ce467fdd1ed6c9f63
Erika.K <koguchi.e915@gmail.com>
2024-11-18 16:01:39 +0900

    [Add]11/18 9章 post_imagesコントローラ追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
+
+  # resources メソッドは、ルーティングを一括して自動生成してくれる機能
+  # 本当はdestroy(投稿削除)、update(投稿更新)、create(投稿作成)も作成されるが
+  # onlyオプションを使用することで、生成するルーティングを限定している
+  resources :post_images, only: [:new, :index, :show]
+
   get "homes/about", to: "homes#about", as: "about"
 end
