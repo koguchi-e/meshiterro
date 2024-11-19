7863d279f2ebd5b3d551f918f6d0d0e295506876
Erika.K <koguchi.e915@gmail.com>
2024-11-19 14:23:33 +0900

    [Add]11/19 14章終

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/edit'
+
   devise_for :users
   root to: "homes#top"
+  get "homes/about", to: "homes#about", as: "about"
 
   # resources メソッドは、ルーティングを一括して自動生成してくれる機能
   # 本当はdestroy(投稿削除)、update(投稿更新)、create(投稿作成)も作成されるが
   # onlyオプションを使用することで、生成するルーティングを限定している
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
-
-  get "homes/about", to: "homes#about", as: "about"
+  resources :users, only:[:show, :edit]
 end
