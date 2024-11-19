cc3ba4fdbc170f094fa8efa900754a61fefb4dd1
Erika.K <koguchi.e915@gmail.com>
2024-11-19 09:22:01 +0900

    [Add]11/19 10章終

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # resources メソッドは、ルーティングを一括して自動生成してくれる機能
   # 本当はdestroy(投稿削除)、update(投稿更新)、create(投稿作成)も作成されるが
   # onlyオプションを使用することで、生成するルーティングを限定している
-  resources :post_images, only: [:new, :index, :show]
+  resources :post_images, only: [:new, :create, :index, :show]
 
   get "homes/about", to: "homes#about", as: "about"
 end
