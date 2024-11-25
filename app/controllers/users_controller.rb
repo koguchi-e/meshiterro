b2f2acd9fa0fbfc15c510039530325771eb41c5f
Erika.K <koguchi.e915@gmail.com>
2024-11-25 11:33:10 +0900

    [Add]11/25 他のユーザーからのアクセスを制限しよう

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,10 +7,12 @@ class UsersController < ApplicationController
   end
 
   def edit
+    is_matching_login_user
     @user = User.find(params[:id])
   end
-
+    
   def update
+    is_matching_login_user
     @user = User.find(params[:id])
     @user.update(user_params)
     redirect_to user_path(@user.id)
@@ -20,6 +22,13 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :profile_image)
+  end
+
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to post_images_path
+    end
   end
 
 end
