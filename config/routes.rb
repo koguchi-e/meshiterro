9fd97d02ecb5d06ff8cc06ee5b2ab28038332a86
Erika.K <koguchi.e915@gmail.com>
2024-12-10 17:48:17 +0900

    [Add] 12/10 管理者機能を実装しよう - 管理者としての機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,38 +2,39 @@ Rails.application.routes.draw do
   get 'users/show'
   get 'users/edit'
 
-  devise_for :admin, skip: [:registrations, :password], controllers: {
+  devise_for :admins, skip: [:registrations, :password], controllers: {
     sessions: 'admin/sessions'
   }
- 
+
   namespace :admin do
     get 'dashboards', to: 'dashboards#index'
-    resources :users, only: [:destroy] 
+    resources :users, only: [:destroy]
+    resources :post_images, only: [:destroy]
+    get 'homes/about', to: 'homes#about', as: 'admin_about'
   end
 
-  devise_for :users
   root to: "homes#top"
-  get "homes/about", to: "homes#about", as: "about"
 
-  # resources メソッドは、ルーティングを一括して自動生成してくれる機能
-  # 本当はdestroy(投稿削除)、update(投稿更新)、create(投稿作成)も作成されるが
-  # onlyオプションを使用することで、生成するルーティングを限定している
+  get "homes/about", to: "homes#about"
+
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
-    resource :favorite, only: [:create, :destroy]
-    # 投稿画像に対してコメントされるため、post_commentsは、post_imagesに結びつける
-    # 親子関係になる
+    resources :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
-  resources :users, only:[:show, :edit, :update]
 
+  resources :users, only: [:show, :edit, :update]
+
+  # ユーザー用の認証ルート
   scope module: :public do
-    devise_for :users
-    root to: 'homes#top'
-    get 'homes/about', to: 'homes#about', as: :about
+    devise_for :users, controllers: {
+      sessions: 'public/sessions'
+    }
+
     resources :post_images, only: [:new, :create, :index, :show, :destroy] do
-      resource :favorites, only: [:create, :destroy]
+      resources :favorites, only: [:create, :destroy]
       resources :post_comments, only: [:create, :destroy]
     end
+
     resources :users, only: [:show, :edit, :update]
   end
 end
