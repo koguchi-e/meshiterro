e48c5e49c9c97f8e25b03891d222af01044233d1
Erika.K <koguchi.e915@gmail.com>
2024-12-10 16:42:39 +0900

    [Add] 12/10 管理者機能を実装しよう - 管理者としての機能追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,6 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!, except: [:top]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-
   # サインイン後にルートパスに遷移しないよう、初期設定を上書きする
   def after_sign_in_path_for(resource)
     post_images_path
@@ -14,6 +13,14 @@ class ApplicationController < ActionController::Base
   # ログアウト後、aboutに転移
   def after_sign_out_path_for(resource)
     about_path
+  end
+
+  # 権限の設定
+
+  private
+ 
+  def admin_controller?
+    self.class.module_parent_name == 'Admin'
   end
 
   protected
