cebf2ce9098887075bb2798143121ea396c2a505
Erika.K <koguchi.e915@gmail.com>
2024-12-11 15:44:03 +0900

    [Add] 12/11 管理者機能を実装しよう - 構成の改善（2）

--- app/controllers/public/application_controller.rb
+++ app/controllers/public/application_controller.rb
@@ -2,25 +2,32 @@
 
   # サインイン後にどこに遷移するかを設定しているメソッド
   # (Deviseが用意しているメソッド) 7章
-  before_action :authenticate_user!, except: [:top]
+  before_action :authenticate_user!, except: [:top], unless: :admin_controller?
   before_action :configure_permitted_parameters, if: :devise_controller?
-
-  # サインイン後にルートパスに遷移しないよう、初期設定を上書きする
-  def after_sign_in_path_for(resource)
-    post_images_path
-  end
-
-  # ログアウト後、aboutに転移
-  def after_sign_out_path_for(resource)
-    about_path
-  end
 
   # 権限の設定
 
   private
+
+  def configure_authentication
+    # 現在のコントローラーがAdmin名前空間の下にあるかどうかを判定する。
+    if admin_controller?
+      # 管理者用の処理
+      authenticate_admin!
+    else
+      # ユーザ用の処理
+      authenticate_user! unless action_is_pubric?
+    end
+  end
  
+  # 管理者用とユーザー用の認証処理を簡単に切り替えることができる
   def admin_controller?
     self.class.module_parent_name == 'Admin'
+  end
+
+  # 特定のアクション（この例ではhomes#top）が認証が不要かどうかを判定します。
+  def action_is_pubric?
+    controller_name == "homes" && action_name == "top"
   end
 
   protected
